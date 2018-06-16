f73308e24365a73ae3bd49b93127871eb28a9f84
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-16 20:16:36 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,5 +12,9 @@ class Transfer
   def valid? 
     @sender.valid? && @receiver.valid? 
   end 
+  
+  def execute_transaction 
+    @sender.balance -= @amount
+    @receiver.balance += @amount
     
 end
