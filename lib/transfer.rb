5fa23019644c28f3030e0e00efaac6fb5f6d20c0
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-16 20:28:29 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,8 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     else 
-      @status = "rejected" 
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
     end 
   end 
   
