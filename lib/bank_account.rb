34bb5a62c40e921bd0f8719a9231150069f42f66
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-16 20:04:22 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,14 @@
 class BankAccount
-
+  attr_reader :name 
+  attr_accessor :balance, :status 
+  
+  def initialize(name) 
+    @name = name 
+    @balance = 1000 
+    @status = "open" 
+  end  
+  
+  def deposit(amount) 
+    @balance += amount
+  end
 end
