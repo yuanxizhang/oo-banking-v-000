ef1442c4d1fa07d9e5dfcaec34289480eb52089a
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-16 20:25:14 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,6 +25,9 @@ class Transfer
   
   def reverse_transfer
     if @status == "complete"
-      
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed" 
+    end
   end
 end
