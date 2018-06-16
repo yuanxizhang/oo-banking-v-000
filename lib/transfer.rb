3b4b9731c5955e0bb561f7fcbd1f5f7f7763da37
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-16 20:12:13 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,11 @@
 class Transfer
   attr_accessor :status 
+  attr_reader :sender, :receiver 
   
   def initialize(sender, receiver, amount) 
-    status = "pending"
+    @status = "pending"
+    @sender = sender
+    @receiver = receiver 
+    @amount = amount
   end 
 end
